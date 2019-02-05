8033999e2ccf59b1bea1e91070a725cfdd111f9b
maciejpmarciniak <maciej.marciniak@kcl.ac.uk>
2019-02-05 20:48:10 +0000

    Exploratory data analysis

--- plotting.py
+++ plotting.py
@@ -4,13 +4,16 @@ import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 from matplotlib.lines import Line2D
 from matplotlib import cm
+import seaborn as sns
 
 
-class Plotting:
+class PlottingCurvature:
+    # -----VentricleVisualization---------------------------------------------------------------------------------------
 
-    def __init__(self, source='/home/mat/Python/data/echo_delineation', ventricle=None):
+    def __init__(self, source='/home/mat/Python/data/echo_delineation', output_path='', ventricle=None):
 
         self.source = source
+        self.output_path = output_path
         self.data = ventricle.data
         self.id = ventricle.id
         self.number_of_frames = ventricle.number_of_frames
@@ -21,7 +24,7 @@ class Plotting:
         self.ed_apex = self.data[self.ed_frame, ventricle.apex*2:ventricle.apex*2+2]
         self.ed_apex_id = ventricle.apex
 
-    def get_translated_element(self, _frame_number, _ref=()):
+    def _get_translated_element(self, _frame_number, _ref=()):
 
         if not np.any(_ref):
             x_ref = np.mean(self.data[_frame_number, ::2])
@@ -40,7 +43,7 @@ class Plotting:
 
     def plot_single_frame(self, frame_number=0):
 
-        xx, yy, _ = self.get_translated_element(frame_number)
+        xx, yy, _ = self._get_translated_element(frame_number)
 
         fig, ax0 = plt.subplots(figsize=(5, 8))
         ax0.plot(xx, yy, 'gd-', ms=5)
@@ -48,11 +51,12 @@ class Plotting:
         ax0.set_xlim(-40, 45)
         ax0.set_ylim(-45, 55)
         fig.tight_layout()
-        fig.savefig(fname=os.path.join(self.source, '{}_frame_{}'.format(self.id, frame_number)))
+        fig.savefig(fname=os.path.join(self.output_path, '{}_frame_{}'.format(self.id, frame_number)))
+        fig.close()
 
     def plot_single_frame_with_curvature(self, frame_number=0):
 
-        xx, yy, _ = self.get_translated_element(frame_number)
+        xx, yy, _ = self._get_translated_element(frame_number)
         curv = self.append_missing_curvature_values(self.c_normalized[frame_number])
 
         fig, (ax0, ax1) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [3, 5]}, figsize=(13, 8))
@@ -67,7 +71,8 @@ class Plotting:
         ax1.axhline(y=0, color='r', linestyle='--')
 
         fig.tight_layout()
-        fig.savefig(fname=os.path.join(self.source, '{}_frame_{}_with_curv'.format(self.id, frame_number)))
+        fig.savefig(fname=os.path.join(self.output_path, '{}_frame_{}_with_curv'.format(self.id, frame_number)))
+        fig.close()
 
     def plot_all_frames(self, coloring_scheme=None):
         fig, (ax0, ax1) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [3, 5]}, figsize=(13, 8))
@@ -87,12 +92,12 @@ class Plotting:
         ax1.set_ylabel('Curvature $[m^{-1}]$')
 
         if coloring_scheme == 'curvature':
-            xx, yy, _ = self.get_translated_element(self.ed_frame, self.ed_apex)
+            xx, yy, _ = self._get_translated_element(self.ed_frame, self.ed_apex)
             curv = self.append_missing_curvature_values(self.curvature[self.ed_frame])
             ax0.plot(xx, yy, 'k--', lw=3)
             ax1.plot(curv, '--', c='black', lw=2)
 
-            xx, yy, _ = self.get_translated_element(self.es_frame, self.ed_apex)
+            xx, yy, _ = self._get_translated_element(self.es_frame, self.ed_apex)
             curv = self.append_missing_curvature_values(self.curvature[self.es_frame])
             ax0.plot(xx, yy, 'k:', lw=3)
             ax1.plot(curv, ':', c='black', lw=2)
@@ -121,7 +126,7 @@ class Plotting:
 
         for frame_number in range(self.number_of_frames):
 
-            xx, yy, _ = self.get_translated_element(frame_number, self.ed_apex)
+            xx, yy, _ = self._get_translated_element(frame_number, self.ed_apex)
             curv = self.append_missing_curvature_values(self.curvature[frame_number])
             norm_curv = self.append_missing_curvature_values(self.c_normalized[self.ed_frame])
             if coloring_scheme == 'curvature':
@@ -152,4 +157,91 @@ class Plotting:
         ax0.legend(handles=legend_elements0, loc='upper left', title='Cardiac cycle')
         ax1.legend(handles=legend_elements1, loc='upper right', title='Curvature')
         fig.tight_layout()
-        fig.savefig(fname=os.path.join(self.source, '{}_colour_by_{}'.format(self.id, ext)))
+        fig.savefig(fname=os.path.join(self.output_path, '{}_colour_by_{}'.format(self.id, ext)))
+        plt.close()
+
+# -----END-VentricleVisualization---------------------------------------------------------------------------------------
+
+
+class PlottingDistributions:
+    # -----DistributionVisualization------------------------------------------------------------------------------------
+
+    def __init__(self, df, series, output_path):
+        self.df = df
+        self.series = series
+        self.output_path = output_path
+
+    def _save_plot(self, plot_name, plot_data):
+        plot_data.savefig(os.path.join(self.output_path, plot_name))
+        plt.close()
+
+    def set_series(self, series):
+        self.series = series
+
+    def plot_distribution(self, show=False):
+
+        f, axes = plt.subplots(1, 3, figsize=(10, 4))
+        sns.despine(left=True)
+        sns.kdeplot(self.df[self.series], shade=True, color='b', ax=axes[0])
+
+        sns.distplot(self.df[self.series], kde=False, rug=True, color='r', bins=None, ax=axes[1])
+        sns.swarmplot(self.df[self.series], color='g', ax=axes[2])
+        series_max = max(self.df[self.series])
+        xlim = (min(self.df[self.series]) - 0.1 * series_max, series_max + 0.1 * series_max)
+
+        for i in range(3):
+            axes[i].set_xlim(xlim)
+
+        axes[0].set_title('Kernel density distribution')
+        axes[1].set_title('Histogram')
+        axes[2].set_title('Swarm distribution')
+        sns.set()
+        plt.tight_layout()
+
+        if show:
+            plt.show()
+        self._save_plot(self.series + '.png', f)
+
+    def plot_multiple_distributions(self, group_by, show=False):
+
+        unique_labels = np.sort(self.df[group_by].unique())
+        plot_name = 'distribution_' + self.series + '_by_' + group_by
+        _ = plt.figure(figsize=(15, 8))
+        for label in unique_labels:
+            group = self.df[self.df[group_by] == label]
+            _ = sns.distplot(group[self.series], label=str(label), hist=False, rug=True)
+
+        _ = plt.gcf()
+
+        if show:
+            plt.show()
+        self._save_plot(plot_name + '.png', _)
+
+    def plot_multiple_boxplots(self, group_by, hue=None, show=False):
+
+        _ = plt.figure(figsize=(15, 8))
+        _ = sns.boxplot(x=group_by, y=self.series, hue=hue, data=self.df)
+        _ = plt.gcf()
+
+        if show:
+            plt.show()
+        plot_name = 'boxplot_' + self.series + '_by_' + group_by
+
+        if hue is not None:
+            plot_name += '_on_' + hue
+        self._save_plot(plot_name + '.png', _)
+
+    def plot_2_distributions(self, series1, series2, kind, show=False):
+
+        g = sns.jointplot(series1, series2, data=self.df, kind=kind,
+                          color='g', height=6, space=0)
+        g.plot_joint(plt.scatter, c="grey", s=20, linewidth=1, marker="+")
+        g.ax_joint.collections[0].set_alpha(0)
+        g.fig.suptitle('2D distribution')
+        sns.set()
+        plt.tight_layout()
+
+        if show:
+            plt.show()
+        self._save_plot(series1 + '_vs_' + series2 + '_' + kind + '.png', g)
+
--- ventricle.py
+++ ventricle.py
@@ -3,8 +3,9 @@ import pandas as pd
 import glob
 import os
 from curvature import Curvature
-from plotting import Plotting
+from plotting import PlottingCurvature, PlottingDistributions
 import matplotlib.pyplot as plt
+from itertools import combinations
 
 
 class Ventricle:
@@ -17,16 +18,17 @@ class Ventricle:
         self.es_frame = 0
         self.ed_frame = 0
         self.apex = 0
-        self.data = self.read_echopac_output()
         self.ventricle_curvature = []
         self.vc_normalized = []  # ventricle_curvature normalized
         self.apices = []
+        self.data = self._read_echopac_output()
+        self.biomarkers = pd.DataFrame(index=[self.id])
         self.get_curvature_per_frame()
         self.get_normalized_curvature()
         self.find_apices()
         self.find_ed_and_es_frame()
 
-    def read_echopac_output(self):
+    def _read_echopac_output(self):
         with open(self.case_name) as f:
             for line in f:
                 if 'ID=' in line:
@@ -41,14 +43,14 @@ class Ventricle:
         return data
 
     @staticmethod
-    def plane_area(x, y):
+    def _plane_area(x, y):
         return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
 
     def find_ed_and_es_frame(self):
         areas = np.zeros(self.number_of_frames)
         for frame in range(self.number_of_frames):
             x, y = self.data[frame, ::2], self.data[frame, 1::2]
-            areas[frame] = self.plane_area(x, y)
+            areas[frame] = self._plane_area(x, y)
         self.es_frame, self.ed_frame = np.argmin(areas), np.argmax(areas)
 
     def get_curvature_per_frame(self):
@@ -65,33 +67,109 @@ class Ventricle:
         values, counts = np.unique(self.apices, return_counts=True)
         self.apex = values[np.argmax(counts)]  # Lowest point in all frames
         # self.apex = self.apices[self.ed_frame]  # Lowest point at end diastole
-        print(self.apex)
-        print(self.number_of_points)
+        # print(self.apex)
+        # print(self.number_of_points)
 
     def get_normalized_curvature(self):
-        # Empirically established values
+        # Empirically established values. Useful for coloring, where values cannot be negative.
         self.vc_normalized = [(single_curvature+0.125)*4 for single_curvature in self.ventricle_curvature]
 
-    def move_to_apex(self):
-        pass
+    def get_biomarkers(self):
+
+        curv = pd.DataFrame(data=self.ventricle_curvature)
+        curv_min_col = curv.min(axis=0).idxmin()
+        curv_max_col = curv.max(axis=0).idxmax()
+        curv_min_row = curv.min(axis=1).idxmin()
+
+        self.biomarkers['min'] = curv.min().min()
+        self.biomarkers['max'] = curv.max().max()
+        self.biomarkers['min_delta'] = np.abs(curv[curv_min_col].max() - self.biomarkers['min'])
+        self.biomarkers['max_delta'] = np.abs(curv[curv_max_col].min() - self.biomarkers['max'])
+        self.biomarkers['min_index'] = np.abs(self.biomarkers['min_delta'] / self.biomarkers['min'])
+        self.biomarkers['max_index'] = np.abs(self.biomarkers['max_delta'] / self.biomarkers['max'])
+        self.biomarkers['amplitude_at_t'] = np.abs(curv.loc[curv_min_row].max() - self.biomarkers['min'])
+        self.biomarkers['min_v_amp_index'] = self.biomarkers['min_delta'] / self.biomarkers['amplitude_at_t']
+        self.biomarkers['delta_ratio'] = self.biomarkers['min_delta'] / self.biomarkers['max_delta']
+
+        return self.biomarkers
+
+
+class Cohort:
+
+    def __init__(self, source_path='data', view='4C', output_path='data', output='all_cases.csv'):
+        self.source_path = source_path
+        self.view = view
+        self.output_path = output_path
+        self.output = output
+        self.files = glob.glob(os.path.join(self.source_path, '*.CSV'))
+        self.files.sort()
+        self.df_all_cases = None
+        self.curv = None
+        self._check_directory(self.output_path)
+        self._check_directory(os.path.join(self.output_path, self.view))
+
+    @staticmethod
+    def _check_directory(directory):
+        if not os.path.isdir(directory):
+            os.mkdir(directory)
+
+    def _try_get_data(self):
+
+        data_file = os.path.join(self.output_path, self.view, 'output_EDA', self.output)
+        if not os.path.exists(data_file):
+            self._build_data_set(to_file=True)
+
+        if self.df_all_cases is None:
+            self.df_all_cases = pd.read_csv(data_file, header=0, index_col=0)
+
+    def _build_data_set(self, to_file=False):
+
+        list_of_dfs = []
+        for curv_file in self.files:
+            print('case: {}'.format(curv_file))
+            ven = Ventricle(curv_file)
+            list_of_dfs.append(ven.get_biomarkers())
+
+        self.df_all_cases = pd.concat(list_of_dfs)
+        if to_file:
+            self.df_all_cases.to_csv(os.path.join(self.output_path, self.view, self.output))
+
+    def plot_curvatures(self):
+
+        c_scheme = 'curvature'
+        self._check_directory(os.path.join(self.output_path, self.view, 'output_' + c_scheme))
+
+        for case in self.files:
+            ven = Ventricle(case_name=case)
+            print(ven.id)
+            print('Points: {}'.format(ven.number_of_points))
+            plot_tool = PlottingCurvature(source=self.source_path,
+                                          output_path=os.path.join(self.output_path, self.view, 'output_' + c_scheme),
+                                          ventricle=ven)
+            plot_tool.plot_all_frames(coloring_scheme=c_scheme)
+
+    def plot_distributions(self):
+
+        if self.df_all_cases is None:
+            self._try_get_data()
+        self._check_directory(os.path.join(self.output_path, self.view, 'output_EDA'))
+
+        plot_tool = PlottingDistributions(self.df_all_cases, '', os.path.join(self.output_path, self.view, 'output_EDA'))
+        for col in self.df_all_cases.columns:
+            plot_tool.set_series(col)
+            plot_tool.plot_distribution()
+
+        col_combs = combinations(self.df_all_cases.columns, 2)
+        for comb in col_combs:
+            plot_tool.plot_2_distributions(comb[0], comb[1], kind='kde')
 
 
 if __name__ == '__main__':
 
-    source = '/home/mat/Python/data/echo_delineation'
-    cases = glob.glob(os.path.join(source, '*.CSV'))
-    cases.sort()
+    view = '2C'
+    source = os.path.join('/home/mat/Python/data/curvature', view)
+    target_path = os.path.join('/home/mat/Python/data/curvature/')
 
-    max_curves = []
-    min_curves = []
-    for case in cases:
-        ven = Ventricle(case_name=case)
-        print(ven.id)
-        # print('Points: {}'.format(ven.number_of_points))
-        plotting = Plotting(source=source, ventricle=ven)
-        plotting.plot_all_frames(coloring_scheme='curvture')
-
-    # plt.plot(max_curves)
-    # plt.plot(min_curves)
-    # print(min(min_curves), min_curves.index(min(min_curves)))
-    # plt.show()
+    cohort = Cohort(source_path=source, view=view, output_path=target_path)
+    # cohort.plot_curvatures()
+    cohort.plot_distributions()
