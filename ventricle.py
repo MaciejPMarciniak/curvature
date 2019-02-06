639dd1639b82a02b223191cc33a8c4eba8bc256d
maciejpmarciniak <maciej.marciniak@kcl.ac.uk>
2019-02-06 20:58:06 +0000

    Cleaning

--- ventricle.py
+++ ventricle.py
@@ -77,15 +77,21 @@ class Ventricle:
 
         curv = pd.DataFrame(data=self.ventricle_curvature)
 
-        _t = int(self.view == '4C')
-        lower_bound = int(np.round((_t * 0.04 + (1 - _t) * 0.96) * len(curv.columns)))
-        upper_bound = int(np.round((_t * 0.4 + (1 - _t) * 0.6) * len(curv.columns)))
-
-        curv_min_col = curv.loc[:, lower_bound:upper_bound].min(axis=0).idxmin()
-        # id of the column (point number) with minimum value
         curv_max_col = curv.max(axis=0).idxmax()
-        curv_min_row = curv.loc[:, lower_bound:upper_bound].min(axis=1).idxmin()
-        # id of the row (frame) with minimum value
+
+        if self.view == '2C':
+            curv_min_col = curv.min(axis=0).idxmin()
+            curv_min_row = curv.min(axis=1).idxmin()
+        else:
+            _t = int(self.view == '4C')
+            lower_bound = int(np.round((_t * 0.04 + (1 - _t) * 0.6) * len(curv.columns)))
+            upper_bound = int(np.round((_t * 0.4 + (1 - _t) * 0.96) * len(curv.columns)))
+
+            # id of the column (point number) with minimum value
+            curv_min_col = curv.loc[:, lower_bound:upper_bound].min(axis=0).idxmin()
+
+            # id of the row (frame) with minimum value
+            curv_min_row = curv.loc[:, lower_bound:upper_bound].min(axis=1).idxmin()
 
         self.biomarkers['min'] = curv.min().min()
         self.biomarkers['max'] = curv.max().max()
@@ -99,14 +105,23 @@ class Ventricle:
 class Cohort:
 
     def __init__(self, source_path='data', view='4C', output_path='data', output='_all_cases.csv'):
-        self.source_path = source_path
+
         self.view = view
+        self.source_path = source_path
         self.output_path = self._check_directory(output_path)
         self.output = output
-        self.files = glob.glob(os.path.join(self.source_path, '*.CSV'))
+        self.files = glob.glob(os.path.join(self.source_path, self.view, '*.CSV'))
         self.files.sort()
         self.df_all_cases = None
+        self.df_master = None
         self.curv = None
+        self.biomarkers = None
+
+    def _set_paths_and_files(self, view='4C', output_path=''):
+        self.view = view
+        self.files = glob.glob(os.path.join(self.source_path, self.view, '*.CSV'))
+        self.files.sort()
+        self.output_path = self._check_directory(output_path)
 
     @staticmethod
     def _check_directory(directory):
@@ -114,18 +129,36 @@ class Cohort:
             os.mkdir(directory)
         return directory
 
-    def _try_get_data(self):
+    def _try_get_data(self, data=False, master_table=False):
 
-        data_file = os.path.join(self.output_path, self.view, 'output_EDA', self.output)
+        if data:
+            _data_file = os.path.join(self.output_path, self.view, 'output_EDA', self.output)
 
-        if os.path.isfile(data_file):
-            self.df_all_cases = pd.read_csv(data_file, header=0, index_col=0)
+            if os.path.isfile(_data_file):
+                self.df_all_cases = pd.read_csv(_data_file, header=0, index_col=0)
 
-        if not os.path.exists(data_file):
-            self._build_data_set(to_file=True)
 
-        if self.df_all_cases is None:
-            self.df_all_cases = pd.read_csv(data_file, header=0, index_col=0)
+            if not os.path.exists(_data_file):
+                self._build_data_set(to_file=True)
+            self.biomarkers = self.df_all_cases.columns
+
+            # if self.df_all_cases is None:
+            #     self.df_all_cases = pd.read_csv(_data_file, header=0, index_col=0)
+
+        if master_table:
+            _master_table_file = os.path.join(self.output_path, 'master_table.csv')
+
+            if os.path.isfile(_master_table_file):
+                self.df_master = pd.read_csv(_master_table_file, header=0, index_col=0)
+
+            if not os.path.exists(_master_table_file):
+                self._build_master_table(to_file=True)
+
+            # if self.df_master is None:
+            #     self.df_master = pd.read_csv(_master_table_file, header=0, index_col=0)
+
+        if not (data or master_table):
+            exit('No data has been created, set the data or master_table parameter to True')
 
     def _build_data_set(self, to_file=False):
 
@@ -136,50 +169,76 @@ class Cohort:
             list_of_dfs.append(ven.get_biomarkers())
 
         self.df_all_cases = pd.concat(list_of_dfs)
-        self.df_all_cases['min_index'] = np.abs(self.df_all_cases['min_delta'] / self.df_all_cases['min'])
-        # self.df_all_cases['max_index'] = np.abs(self.df_all_cases['max_delta'] / self.df_all_cases['max'])
-        self.df_all_cases['min_index2'] = np.abs(self.df_all_cases['min_delta'] * self.df_all_cases['min']) * 1000
-        self.df_all_cases['log_min_index2'] = np.log(self.df_all_cases['min_index2'])
-        # self.df_all_cases['max_index2'] = np.abs(self.df_all_cases['max_delta'] * self.df_all_cases['max'])
-        # self.df_all_cases['min_v_amp_index'] = self.df_all_cases['min_delta'] / self.df_all_cases['amplitude_at_t']
-        self.df_all_cases['min_v_amp_index2'] = self.df_all_cases['min_delta'] * self.df_all_cases['amplitude_at_t'] * 1000
-        self.df_all_cases['log_min_v_amp_index2'] = np.log(self.df_all_cases['min_v_amp_index2'])
+        self.df_all_cases['min_index2'] = np.abs(self.df_all_cases['min_delta'] / self.df_all_cases['min'])
+        self.df_all_cases['min_index'] = np.abs(self.df_all_cases['min_delta'] * self.df_all_cases['min']) * 1000
+        self.df_all_cases['log_min_index'] = np.log(self.df_all_cases['min_index'])
+        self.df_all_cases['min_v_amp_index'] = self.df_all_cases['min_delta'] * self.df_all_cases['amplitude_at_t']*1000
+        self.df_all_cases['log_min_v_amp_index'] = np.log(self.df_all_cases['min_v_amp_index'])
         self.df_all_cases['delta_ratio'] = self.df_all_cases['min_delta'] / self.df_all_cases['max_delta']
 
         if to_file:
             self.df_all_cases.to_csv(os.path.join(self.output_path, self.view, 'output_EDA', self.output))
 
+    def _build_master_table(self, to_file=False, views=('4C', '3C', '2C')):
+
+        list_of_dfs = []
+
+        for view in views:
+            self._set_paths_and_files(view=view, output_path=self.output_path)
+            self._try_get_data(data=True)
+            self.df_all_cases.columns = [self.view + '_' + col for col in self.df_all_cases.columns]
+            list_of_dfs.append(self.df_all_cases)
+
+        self.df_master = list_of_dfs[0]
+        for df_i in range(1, len(list_of_dfs)):
+            self.df_master = self.df_master.merge(list_of_dfs[df_i], right_index=True, left_index=True, sort=True)
+        if to_file:
+            self.df_master.to_csv(os.path.join(self.output_path, 'master_table.csv'))
+
     def plot_curvatures(self, coloring_scheme='curvature'):
 
+        _source_path = os.path.join(self.source_path, self.view)
         _output_path = self._check_directory(os.path.join(self.output_path, self.view, 'output_curvature'))
 
         for case in self.files:
             ven = Ventricle(case_name=case, view=self.view)
             print(ven.id)
             print('Points: {}'.format(ven.number_of_points))
-            plot_tool = PlottingCurvature(source=self.source_path,
+            plot_tool = PlottingCurvature(source=_source_path,
                                           output_path=_output_path,
                                           ventricle=ven)
             plot_tool.plot_all_frames(coloring_scheme=coloring_scheme)
 
-    def plot_distributions(self):
+    def plot_distributions(self, plot_data=False, plot_master=False):
 
-        if self.df_all_cases is None:
-            self._try_get_data()
-        _output_path = self._check_directory(os.path.join(self.output_path, self.view, 'output_EDA'))
+        if plot_data:
+            if self.df_all_cases is None:
+                self._try_get_data(data=True)
 
-        plot_tool = PlottingDistributions(self.df_all_cases, '', _output_path)
-        for col in self.df_all_cases.columns:
-            plot_tool.set_series(col)
-            plot_tool.plot_distribution()
+            _view_output_path = self._check_directory(os.path.join(self.output_path, self.view, 'output_EDA'))
 
-        col_combs = combinations(self.df_all_cases.columns, 2)
-        for comb in col_combs:
-            plot_tool.plot_2_distributions(comb[0], comb[1], kind='kde')
+            plot_tool = PlottingDistributions(self.df_all_cases, '', _view_output_path)
+            for col in self.biomarkers:
+                plot_tool.set_series(col)
+                plot_tool.plot_distribution()
+
+            col_combs = combinations(self.biomarkers, 2)
+            for comb in col_combs:
+                plot_tool.plot_2_distributions(comb[0], comb[1], kind='kde')
+
+        if plot_master:
+            if self.df_master is None:
+                self._try_get_data(master_table=True)
+
+            _master_output_path = self._check_directory(os.path.join(self.output_path, 'output_master'))
+            master_plot_tool = PlottingDistributions(self.df_master, '', _master_output_path)
+
+            for col in self.biomarkers:
+                master_plot_tool.plot_2_distributions('4C_'+col, '3C_'+col, kind='kde')
 
     def get_extemes(self, n=30):
 
-        self._try_get_data()
+        self._try_get_data(data=True)
 
         list_of_extremes = []
         for col in self.df_all_cases.columns:
@@ -196,13 +255,14 @@ class Cohort:
 
 if __name__ == '__main__':
 
-    for _view in ['3C', '2C']:
+    for _view in ['2C']:
 
-        source = os.path.join('/home/mat/Python/data/curvature', _view)
+        source = os.path.join('/home/mat/Python/data/curvature')
         target_path = os.path.join('/home/mat/Python/data/curvature/')
 
         cohort = Cohort(source_path=source, view=_view, output_path=target_path)
-        cohort.get_extemes(32)
-        cohort.plot_curvatures('asf')
-        cohort.plot_curvatures()
-        cohort.plot_distributions()
+
+        # cohort.get_extemes(32)
+        # cohort.plot_curvatures('asf')
+        # cohort.plot_curvatures()
+        cohort.plot_distributions(plot_master=True)
